6baf299607f9f901954d146a32296f9a02bbe2a3
liuyulvv <liuyulvv@outlook.com>
2022-10-30 09:11:26 +0800

    禁用warning

--- include/spdlog/fmt/bundled/locale.h
+++ include/spdlog/fmt/bundled/locale.h
@@ -1,2 +1,3 @@
 #include "xchar.h"
-#warning fmt/locale.h is deprecated, include fmt/format.h or fmt/xchar.h instead
+// liuyulvv@2022.10.30: disable warning
+// #warning fmt/locale.h is deprecated, include fmt/format.h or fmt/xchar.h instead
